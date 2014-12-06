b749133540e85f7fdb32145ed345004d523e6ec3
Yann Malet <yann.malet@gmail.com>
2014-12-06 12:02:04 +0100

    Initial consumer architecture mainly adapted from nsq_tail.go.

---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/bitly/go-nsq"
+	"github.com/bitly/nsq/util"
+)
+
+var (
+	showVersion = flag.Bool("version", false, "print version string")
+	topic       = flag.String("topic", "", "NSQ topic")
+	channel     = flag.String("channel", "", "NSQ channel")
+
+	maxInFlight   = flag.Int("max-in-flight", 200, "max number of messages to allow in flight")
+	totalMessages = flag.Int("n", 0, "total messages to show (will wait if starved)")
+
+	consumerOpts     = util.StringArray{}
+	nsqdTCPAddrs     = util.StringArray{}
+	lookupdHTTPAddrs = util.StringArray{}
+)
+
+func init() {
+	flag.Var(&consumerOpts, "consumer-opt", "option to passthrough to nsq.Consumer (may be given multiple times, http://godoc.org/github.com/bitly/go-nsq#Config)")
+
+	flag.Var(&nsqdTCPAddrs, "nsqd-tcp-address", "nsqd TCP address (may be given multiple times)")
+	flag.Var(&lookupdHTTPAddrs, "lookupd-http-address", "lookupd HTTP address (may be given multiple times)")
+}
+
+type ThumbnailerHandler struct {
+	sourceImage      string
+	thumbnailCounter int
+}
+
+func (th *ThumbnailerHandler) HandleMessage(m *nsq.Message) error {
+	_, err := os.Stdout.Write(m.Body)
+	if err != nil {
+		log.Fatalf("ERROR: failed to write to os.Stdout - %s", err)
+	}
+	_, err = os.Stdout.WriteString("\n")
+	if err != nil {
+		log.Fatalf("ERROR: failed to write to os.Stdout - %s", err)
+	}
+	return nil
+}
+
+func main() {
+	log.Println("Starting the thumbnailing consumer")
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf("nsq_thumbnailer v%s\n", util.BINARY_VERSION)
+		return
+	}
+
+	if *channel == "" {
+		rand.Seed(time.Now().UnixNano())
+		*channel = fmt.Sprintf("tail%06d#ephemeral", rand.Int()%999999)
+	}
+
+	if *topic == "" {
+		log.Fatal("--topic is required")
+	}
+
+	if len(nsqdTCPAddrs) == 0 && len(lookupdHTTPAddrs) == 0 {
+		log.Fatal("--nsqd-tcp-address or --lookupd-http-address required")
+	}
+	if len(nsqdTCPAddrs) > 0 && len(lookupdHTTPAddrs) > 0 {
+		log.Fatal("use --nsqd-tcp-address or --lookupd-http-address not both")
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Don't ask for more messages than we want
+	if *totalMessages > 0 && *totalMessages < *maxInFlight {
+		*maxInFlight = *totalMessages
+	}
+
+	cfg := nsq.NewConfig()
+	cfg.UserAgent = fmt.Sprintf("nsq_tail/%s go-nsq/%s", util.BINARY_VERSION, nsq.VERSION)
+	err := util.ParseOpts(cfg, consumerOpts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg.MaxInFlight = *maxInFlight
+
+	consumer, err := nsq.NewConsumer(*topic, *channel, cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	consumer.AddHandler(&ThumbnailerHandler{})
+
+	err = consumer.ConnectToNSQDs(nsqdTCPAddrs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = consumer.ConnectToNSQLookupds(lookupdHTTPAddrs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		select {
+		case <-consumer.StopChan:
+			return
+		case <-sigChan:
+			consumer.Stop()
+		}
+	}
+
+}
