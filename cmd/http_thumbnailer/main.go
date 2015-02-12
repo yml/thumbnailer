76e53c79c6f28fd170ee189531fbe2a71c387c70
Yann Malet <yann.malet@gmail.com>
2015-02-12 10:39:38 +0100

    Intermediate commit to add /thumb/ end point

--- cmd/http_thumbnailer/main.go
+++ cmd/http_thumbnailer/main.go
@@ -8,6 +8,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"strings"
 
@@ -15,16 +16,34 @@ import (
 )
 
 var (
-	addr     = flag.String("addr", "127.0.0.1:9900", "http addr (default is 127.0.0.1:9900)")
-	URLNames = make(map[string]string)
+	addr      = flag.String("addr", "127.0.0.1:9900", "http addr (default is 127.0.0.1:9900)")
+	srcFolder = flag.String("srcFolder", "", "Source folder including the scheme (file:///tmp/my.jpg)")
+	dstFolder = flag.String("dstFolder", "", "Destination folder including the scheme (file:///tmp/my.jpg)")
+	URLNames  = make(map[string]string)
 )
+
+func thumbHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		var width, height int
+		var filename string
+		path := strings.TrimPrefix(r.URL.Path, URLNames["/thumb/"])
+		fmt.Sscanf(path, "%dx%d/%s", &width, &height, &filename)
+		fmt.Printf("[DEBUG] width: %d , height: %d, filename: %s ", width, height, filename)
+		// build the thumbReg and generate the thumb and return it or redirect
+
+	} else {
+		http.Error(w, fmt.Sprintf("Request method not supported: %s", r.Method), http.StatusBadRequest)
+		return
+	}
+
+}
 
 func thumbsHandler(w http.ResponseWriter, r *http.Request) {
 	var thumbReq bytes.Buffer
 	if r.Method == "GET" {
 		// In this case we are going to look for the thumbReq in the URL.
 		// It should be base 64 encoded
-		path := strings.TrimPrefix(r.URL.Path, URLNames["/thumb/"])
+		path := strings.TrimPrefix(r.URL.Path, URLNames["/thumbs/"])
 		_, err := io.Copy(&thumbReq, base64.NewDecoder(base64.URLEncoding, strings.NewReader(path)))
 		if err != nil {
 			http.Error(w, fmt.Sprintf("failed to decode the thumb generation request: %s", err), http.StatusBadRequest)
@@ -89,22 +108,33 @@ func main() {
 	fmt.Println("Starting HTTP thumbnailer on: ", *addr)
 	URLNames["/"] = "/"
 	URLNames["/thumbs/"] = "/thumbs/"
-	URLNames["/debug/"] = "/debug/"
+	URLNames["/thumb/"] = "/thumb/"
+	URLNames["/base64Encode/"] = "/debug_base64Encode/"
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("This is the home page"))
 	})
-	mux.HandleFunc(URLNames["/debug/"], func(w http.ResponseWriter, r *http.Request) {
-		thumbReq := []byte("{\"srcImage\": \"file:///home/yml/Dropbox/Devs/golang/nsq_sandbox/nsq-thumb-src-test/baignade.jpg\", \"opts\": [{\"rect\":{\"min\":[200, 200], \"max\":[600,600]},\"width\":150, \"height\":0}], \"dstFolder\":\"file:///home/yml/Dropbox/Devs/golang/nsq_sandbox/nsq-thumb-dst-test/\"}")
-		fmt.Printf("thumbReq : %s\n", thumbReq)
-		w.Write([]byte("\n"))
-		enc := base64.NewEncoder(base64.URLEncoding, w)
-		enc.Write(thumbReq)
-		enc.Close()
-		w.Write([]byte("\n\n"))
+	mux.HandleFunc(URLNames["/base64Encode/"], func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			thumbReq, err := ioutil.ReadAll(r.Body)
+			defer r.Body.Close()
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+
+			w.Write([]byte("\n"))
+			enc := base64.NewEncoder(base64.URLEncoding, w)
+			enc.Write(thumbReq)
+			enc.Close()
+			w.Write([]byte("\n"))
+		} else {
+			http.Error(w, "This request.Method is not implemented for this endpoint", http.StatusInternalServerError)
+		}
 	})
 	mux.HandleFunc(URLNames["/thumbs/"], thumbsHandler)
+	mux.HandleFunc(URLNames["/thumb/"], thumbHandler)
 	http.ListenAndServe(*addr, mux)
 
 }
