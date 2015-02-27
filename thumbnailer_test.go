df379603ead4550b7161365b77d5899fe7b50811
Yann Malet <yann.malet@gmail.com>
2015-02-27 14:07:05 +0100

    Adding a test.
    go test -cover -v github.com/yml/thumbnailer/...
    
    --- PASS: TestGenerateThumbnails (0.30s)
    PASS
    coverage: 47.5% of statements
    ok      github.com/yml/thumbnailer      0.308s  coverage: 47.5% of
    statements

---
+++ thumbnailer_test.go
@@ -0,0 +1,33 @@
+package thumbnailer
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestGenerateThumbnails(t *testing.T) {
+	pwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal("Failed to get the current directory: %s", err)
+	}
+	fmt.Println("Current dir:", pwd)
+	opt := ThumbnailOpt{Width: 100, Height: 100}
+	opts := make([]ThumbnailOpt, 0)
+	opts = append(opts, opt)
+	tm := ThumbnailerMessage{
+		SrcImage:  filepath.Join("file://", pwd, "testdata", "pic.jpg"),
+		DstFolder: "file:///tmp",
+		Opts:      opts}
+	results := tm.GenerateThumbnails()
+	for result := range results {
+		if result.Err != nil {
+			t.Fatal("An error occured while generating a thumb :", result.Err)
+		}
+		// Clean up the generated thumb
+		if err := os.Remove(result.Thumbnail.Path); err != nil {
+			t.Fatal("Failed to delete the generated thumb:", err)
+		}
+	}
+}
